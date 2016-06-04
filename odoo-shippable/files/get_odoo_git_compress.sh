8f221627366738fb308abe15c6f1acefa5b3d19c
Moisés López <moylop260@vauxoo.com>
2016-06-04 18:09:55 -0500

    [REF] odoo-shippable: Migrate url from dropbox to amazon s3
    Signed-off-by: Moisés López <moylop260@vauxoo.com>

--- odoo-shippable/files/get_odoo_git_compress.sh
+++ odoo-shippable/files/get_odoo_git_compress.sh
@@ -9,15 +9,5 @@ git init --bare ${HOME}/odoo_git_compress/.git \
  && unbuffer git --git-dir=${HOME}/odoo_git_compress/.git gc --aggressive \
  && du -sh ${HOME}/odoo_git_compress/.git \
  && rm -f ${HOME}/odoo_git.tar.gz \
- && tar -zcvf ${HOME}/odoo_git.tar.gz ${HOME}/odoo_git_compress/.git \
+ && tar -zcvf ${HOME}/odoo_git.tar.gz -C ${HOME}/odoo_git_compress .git \
  && echo "Now you can upload the file ${HOME}/odoo_git.tar.gz"
-
- # && mkdir -p ${HOME}/addons-vauxoo/ \
- # && git init ${HOME}/addons-vauxoo/ \
- # && git --git-dir=${HOME}/addons-vauxoo/.git remote add vauxoo https://github.com/vauxoo/addons-vauxoo.git \
- # && git --git-dir=${HOME}/addons-vauxoo/.git fetch --all \
- # && du -sh ${HOME}/addons-vauxoo/.git \
- # && unbuffer git --git-dir=${HOME}/addons-vauxoo/.git gc --aggressive \
- # && du -sh ${HOME}/addons-vauxoo/.git
-
-
