73b767405168d32c06f1b786f3dd9b6e02dfd7f4
Moisés López <moylop260@vauxoo.com>
2017-01-21 12:52:53 -0600

    [REF] odoo-shippable: using depth 1

--- odoo-shippable/scripts/library.sh
+++ odoo-shippable/scripts/library.sh
@@ -6,7 +6,7 @@ git_clone_copy(){
     WHAT="${3}"
     WHERE="${4}"
     TEMPDIR="$( mktemp -d )"
-    OPTIONS="--depth 30 -b ${BRANCH} -q --single-branch --recursive"
+    OPTIONS="--depth 1 -b ${BRANCH} -q --single-branch --recursive"
     echo "Cloning ${URL} ..."
     mkdir -p $( dirname "${WHERE}" )
     git clone ${URL} ${OPTIONS} ${TEMPDIR}
@@ -31,10 +31,10 @@ git_clone_execute(){
     BRANCH="${2}"
     SCRIPT="${3}"
     TEMPDIR="$( mktemp -d )"
-    OPTIONS="--depth 30 -b ${BRANCH} -q --single-branch --recursive"
+    OPTIONS="--depth 1 -b ${BRANCH} -q --single-branch --recursive"
     echo "Cloning ${URL} ..."
     git clone ${URL} ${OPTIONS} ${TEMPDIR}
-    bash "${TEMPDIR}/${SCRIPT}"
+    (cd ${TEMPDIR} && ./${SCRIPT})
     rm -rf ${TEMPDIR}
 }
 
