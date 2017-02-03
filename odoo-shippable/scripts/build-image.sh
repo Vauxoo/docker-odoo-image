3ec5fb4601a94d52c7e287c3d5507694dece9adf
JesusZapata <jesus@vauxoo.com>
2017-02-03 12:10:03 -0400

    [ADD] odoo-shipeable: Disable auto trigger for youcompleteme

--- odoo-shippable/scripts/build-image.sh
+++ odoo-shippable/scripts/build-image.sh
@@ -140,10 +140,14 @@ git_clone_copy "${VIM_OPENERP_REPO}" "master" "vim/" "${HOME}/.vim/bundle/vim-op
 sed -i 's/ set mouse\=a/\"set mouse\=a/g' ~/.vimrc
 sed -i "s/let g:neocomplete#enable_at_startup = 1/let g:neocomplete#enable_at_startup = 0/g" ~/.vimrc
 
-# Install YouCompleteMe
+# Install and configure YouCompleteMe
 VIM_YOUCOMPLETEME_PATH="${HOME}/.vim/bundle/YouCompleteMe"
 git_clone_copy "${VIM_YOUCOMPLETEME_REPO}" "master" "." "${VIM_YOUCOMPLETEME_PATH}"
 (cd "${VIM_YOUCOMPLETEME_PATH}" && ./install.py)
+cat >> ~/.vimrc << EOF
+# Disable auto trigger for youcompleteme
+let g:ycm_auto_trigger = 0
+EOF
 
 # Install WakaTime
 git_clone_copy "${VIM_WAKATIME_REPO}" "master" "." "${HOME}/.vim/bundle/vim-wakatime"
