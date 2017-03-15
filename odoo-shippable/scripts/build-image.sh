5b287c26d6289826273138a91e8dbd31b785bf2c
Luis Alejandro Martínez Faneyth <luis@huntingbears.com.ve>
2017-03-15 09:00:15 -0400

    [REF] Adding option for Vim to overwrite repeated snipMate snippets with the last one loaded instead of showing warnings. Fixes #199.

--- odoo-shippable/scripts/build-image.sh
+++ odoo-shippable/scripts/build-image.sh
@@ -167,6 +167,12 @@ cat >> ~/.vimrc << EOF
 let g:signify_disable_by_default = 1 
 EOF
 
+# Overwrite repeated snipmate snippets
+cat >> ~/.vimrc << EOF
+" Overwrite repeated snipmate snippets
+let g:snipMate.override = 1
+EOF
+
 # Install and configure YouCompleteMe
 VIM_YOUCOMPLETEME_PATH="${HOME}/.vim/bundle/YouCompleteMe"
 git_clone_copy "${VIM_YOUCOMPLETEME_REPO}" "master" "." "${VIM_YOUCOMPLETEME_PATH}"
