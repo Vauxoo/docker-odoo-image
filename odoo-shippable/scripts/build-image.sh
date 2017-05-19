c3216c2eb0510354e200f4e490075055c587ffa0
JesusZapata <jesus@vauxoo.com>
2017-05-19 10:46:36 -0400

    [FIX] odoo-shippable: Adding dependency html2text to odoo-shippable

--- odoo-shippable/scripts/build-image.sh
+++ odoo-shippable/scripts/build-image.sh
@@ -51,7 +51,8 @@ DPKG_DEPENDS="postgresql-9.3 postgresql-contrib-9.3 postgresql-9.5 postgresql-co
 PIP_OPTS="--upgrade \
           --no-cache-dir"
 PIP_DEPENDS_EXTRA="line-profiler watchdog coveralls diff-highlight \
-                   pg-activity virtualenv nodeenv setuptools==33.1.1"
+                   pg-activity virtualenv nodeenv setuptools==33.1.1 \
+                   html2text==2016.9.19"
 PIP_DPKG_BUILD_DEPENDS=""
 NPM_OPTS="-g"
 NPM_DEPENDS="localtunnel fs-extra eslint"
