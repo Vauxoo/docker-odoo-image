d59ee2df2f51d5816649f3a3cd11eb6522323427
JesusZapata <jesus@vauxoo.com>
2017-06-22 16:12:38 -0400

    [FIX] odoo-shippable: Not install the youcompleteme
    Yoycompleteme required the version g++4.9

--- odoo-shippable/scripts/build-image.sh
+++ odoo-shippable/scripts/build-image.sh
@@ -187,7 +187,8 @@ EOF
 # Install and configure YouCompleteMe
 VIM_YOUCOMPLETEME_PATH="${HOME}/.vim/bundle/YouCompleteMe"
 git_clone_copy "${VIM_YOUCOMPLETEME_REPO}" "master" "." "${VIM_YOUCOMPLETEME_PATH}"
-(cd "${VIM_YOUCOMPLETEME_PATH}" && ./install.py)
+# The version required by youcompleteme is g++4.9
+#(cd "${VIM_YOUCOMPLETEME_PATH}" && ./install.py)
 cat >> ~/.vimrc << EOF
 " Disable auto trigger for youcompleteme
 let g:ycm_auto_trigger = 0
